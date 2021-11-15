b0cb1897c9a1df84b08c634ef4860a12b82d82ea
Caroline Gognet <carolinegognet@gmail.com>
2021-11-15 16:05:36 +0100

    models updates

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :user
   belongs_to :species
+  has_many :reservations
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :address, presence: true
+  validates :price, presence: true
 end
--- app/models/species.rb
+++ app/models/species.rb
@@ -1,2 +1,3 @@
 class Species < ApplicationRecord
+  has_many :animals
 end
