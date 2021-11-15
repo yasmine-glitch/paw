542b28e96338ef71d701c4007ae98cb0cbfeb5a2
Caroline Gognet <carolinegognet@gmail.com>
2021-11-15 17:36:15 +0100

    form animal added

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -20,6 +20,12 @@ class AnimalsController < ApplicationController
     end
   end
 
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirect_to animal_path(@animal)
+  end
+
   private
 
   def animal_params
