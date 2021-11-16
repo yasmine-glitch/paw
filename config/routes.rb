2d482089d356dfdbd159ab4faeaadde53d4b09b4
Amelie Patin <amelie.patin@gmail.com>
2021-11-16 12:34:38 +0100

    reservations controller and views for show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :animals do
-    resources :reservations, only: [ :new, :create ]
+    resources :reservations, only: [ :new, :create, :show ]
   end
   get "/dashboard", to: "pages#dashboard"
   get "/reservations", to: "reservations#index"
