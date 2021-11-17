38bce30dc8e4e85cfa21e66c5f02ba996182e8b5
Caroline Gognet <carolinegognet@gmail.com>
2021-11-17 12:40:40 +0100

    card

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -9,4 +9,6 @@ class Animal < ApplicationRecord
   validates :species, presence: true
   validates :description, presence: true
   validates :quality, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
