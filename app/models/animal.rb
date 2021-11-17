86319ac1374b710e1e0c46ec47dc592fd1636f71
Sam Rozenberg <sam.rozen@hotmail.com>
2021-11-17 15:54:34 +0100

    changes

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -9,7 +9,6 @@ class Animal < ApplicationRecord
   validates :species, presence: true
   validates :description, presence: true
   validates :quality, presence: true
-  validates :photos, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
