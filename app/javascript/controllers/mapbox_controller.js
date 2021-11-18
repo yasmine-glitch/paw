7fefd2f500a0027d8172faf1567232a5b93233f5
Caroline Gognet <carolinegognet@gmail.com>
2021-11-18 14:43:35 +0100

    click on the image on the map ok + footer ok

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -42,6 +42,6 @@ export default class extends Controller {
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 30, duration: 0 });
+    this.map.fitBounds(bounds, { padding: 60, maxZoom: 30, duration: 0 });
   }
 }
