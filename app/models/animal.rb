9afcb2c77cecb91397bbd22fe1c656609a531a69
Yasmine BERRADA <yasmineberrada91@gmail.com>
2021-11-18 13:06:04 +0100

    created searchbar for address, name, species

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -11,4 +11,13 @@ class Animal < ApplicationRecord
   validates :quality, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+# <!--Yas : START code searchbar animal address in animal model -->
+  include PgSearch::Model
+  pg_search_scope :search_by_address_species_name,
+                  against: [:address, :species, :name],
+                  using: {
+                  tsearch: { prefix: true } # <-- now `14 rue Craspin du Gast` will return something!
+                  }
+# <!--Yas : END code searchbar animal address in animal model -->
 end
